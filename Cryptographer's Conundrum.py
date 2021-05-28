8f68d0e9c5242e0e93b24e9669482fd11b74d73f
Prasath  Ram <71248627+PrasathRamR@users.noreply.github.com>
2021-05-28 08:45:07 +0530

    Create Cryptographer's Conundrum.py

---
+++ Cryptographer's Conundrum.py
@@ -0,0 +1,9 @@
+new_cipher = input()
+replacement = "PER"
+replacementIndex = 0
+count = 0
+for index in range(len(new_cipher)):
+    if(cipher[index] != replacement[replacementIndex]):
+        count+=1
+    replacementIndex = (replacementIndex + 1) % 3 
+print(count)
